1de358f1d1967ec87a576615c7b92ab7dafaab38
YDUP <ydup@foxmail.com>
2021-01-24 21:07:28 +0800

    add image

--- bokeh-app/main.py
+++ bokeh-app/main.py
@@ -28,26 +28,26 @@ def create_pannel(id):
     D = 3
     shape = np.array([len(range(1, (len(ecg_y)-1)//(D-1) + 1)), len(range(0, len(ecg_y)-(D-1)*1)), D])
     
-    # # Symmetrized Grad-CAM
-    # xdr = Range1d(start=0, end=3000)
-    # ydr = Range1d(start=1500, end=0)
+    # Symmetrized Grad-CAM
+    xdr = Range1d(start=0, end=3000)
+    ydr = Range1d(start=1500, end=0)
 
-    # pannel_nAF = figure(title='Symmetrized Grad-CAM of non-AF class', x_range=xdr, y_range=ydr, plot_width=400, plot_height=200)
-    # pannel_nAF.image_url(url=['http://yadongz.com/static/img/ecg/gcam_nAF_%s.png'%id], x=0, y=shape[0], w=shape[1], h=shape[0], anchor="bottom_left")
-    # source_triad_img = ColumnDataSource(data=dict(x=[0], y=[0]))
-    # pannel_nAF.scatter('x', 'y', source=source_triad_img, size=15, marker='circle_x',line_color="white", fill_color="none", alpha=1)
-    # pannel_nAF.xaxis.visible = False
-    # pannel_nAF.xgrid.visible = False
-    # pannel_nAF.yaxis.visible = False
-    # pannel_nAF.ygrid.visible = False
+    pannel_nAF = figure(title='Symmetrized Grad-CAM of non-AF class', x_range=xdr, y_range=ydr, plot_width=400, plot_height=200)
+    pannel_nAF.image_url(url=['http://yadongz.com/static/img/ecg/gcam_nAF_%s.png'%id], x=0, y=shape[0], w=shape[1], h=shape[0], anchor="bottom_left")
+    source_triad_img = ColumnDataSource(data=dict(x=[0], y=[0]))
+    pannel_nAF.scatter('x', 'y', source=source_triad_img, size=15, marker='circle_x',line_color="white", fill_color="none", alpha=1)
+    pannel_nAF.xaxis.visible = False
+    pannel_nAF.xgrid.visible = False
+    pannel_nAF.yaxis.visible = False
+    pannel_nAF.ygrid.visible = False
 
-    # pannel_AF = figure(title='Symmetrized Grad-CAM of AF class', x_range=xdr, y_range=ydr, plot_width=400, plot_height=200)
-    # pannel_AF.image_url(url=['http://yadongz.com/static/img/ecg/gcam_AF_%s.png'%id], x=0, y=shape[0], w=shape[1], h=shape[0], anchor="bottom_left")
-    # pannel_AF.scatter('x', 'y', source=source_triad_img, size=15, marker='circle_x',line_color="white", fill_color="none", alpha=1)
-    # pannel_AF.xaxis.visible = False
-    # pannel_AF.xgrid.visible = False
-    # pannel_AF.yaxis.visible = False
-    # pannel_AF.ygrid.visible = False
+    pannel_AF = figure(title='Symmetrized Grad-CAM of AF class', x_range=xdr, y_range=ydr, plot_width=400, plot_height=200)
+    pannel_AF.image_url(url=['http://yadongz.com/static/img/ecg/gcam_AF_%s.png'%id], x=0, y=shape[0], w=shape[1], h=shape[0], anchor="bottom_left")
+    pannel_AF.scatter('x', 'y', source=source_triad_img, size=15, marker='circle_x',line_color="white", fill_color="none", alpha=1)
+    pannel_AF.xaxis.visible = False
+    pannel_AF.xgrid.visible = False
+    pannel_AF.yaxis.visible = False
+    pannel_AF.ygrid.visible = False
 
     # # Set up widgets
     time_index = Slider(title="x", value=0, start=0, end=shape[1], step=1, width=400)
@@ -75,14 +75,14 @@ def create_pannel(id):
     for w in [time_index, delay]:
         w.on_change('value', update_data)
     
-    return time_index, delay, pannel_ts#, pannel_AF, pannel_nAF
+    return time_index, delay, pannel_ts, pannel_AF, pannel_nAF
     
     # return pannel_ts
 
 
-time_index_1, delay_1, pannel_ts_1 = create_pannel('AF')
+time_index_1, delay_1, pannel_ts_1, pannel_AF_1, pannel_nAF_1 = create_pannel('AF')
 inputs_1 = column(time_index_1, delay_1)
-column_1 = column(inputs_1, pannel_ts_1, width=400)
+column_1 = column(inputs_1, pannel_ts_1, pannel_AF_1, pannel_nAF_1, width=400)
 '''
 time_index_2, delay_2, pannel_ts_2, pannel_AF_2, pannel_nAF_2 = create_pannel('nAF')
 inputs_2 = column(time_index_2, delay_2)
