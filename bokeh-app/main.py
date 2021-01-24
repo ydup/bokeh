34940c0d0b993688284018e0fc00c43051d1da0f
YDUP <ydup@foxmail.com>
2021-01-24 21:41:14 +0800

    new

--- bokeh-app/main.py
+++ bokeh-app/main.py
@@ -34,7 +34,7 @@ def create_pannel(id):
     ydr = Range1d(start=1500, end=0)
 
     pannel_nAF = figure(title='Symmetrized Grad-CAM of non-AF class', x_range=xdr, y_range=ydr, plot_width=400, plot_height=200)
-    pannel_nAF.image_url(url=['./bokeh-app/data/gcam_nAF_%s.png'%id], x=0, y=shape[0], w=shape[1], h=shape[0], anchor="bottom_left")
+    pannel_nAF.image_url(url=[join(dirname(__file__),'data/gcam_nAF_%s.png'%id)], x=0, y=shape[0], w=shape[1], h=shape[0], anchor="bottom_left")
     source_triad_img = ColumnDataSource(data=dict(x=[0], y=[0]))
     pannel_nAF.scatter('x', 'y', source=source_triad_img, size=15, marker='circle_x',line_color="white", fill_color="none", alpha=1)
     pannel_nAF.xaxis.visible = False
